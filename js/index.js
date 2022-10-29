9a6b57124e6d1e572d607d9a90e9fe429e32136b
KristennBon <104530306+KristennBon@users.noreply.github.com>
2022-10-28 20:48:08 -0700

    yes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 console.log("your index.js is loading correctly!");
 
-$('.nav').on("click", function(){
-    $('.cssChange').css("background-color","maroon");
+$('.navbar').on("click", function(){
+    $('.cssChange').css("background-color","maroon"); 
+}
