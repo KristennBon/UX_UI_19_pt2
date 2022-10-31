d10180322090b44ce9877b2c95cd2392bac18ac4
KristennBon <104530306+KristennBon@users.noreply.github.com>
2022-10-30 18:00:36 -0700

    changes

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ console.log("your index.js is loading correctly!");
 $('.navbar').on("click", function(){
     $('.cssChange').css("background-color","maroon"); 
 }
+colorLinks("#00FF00");
+
+function colorLinks(hex)
+{
+    var links = document.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++)
+    {
+        if(links[i].href)
+        {
+            links[i].style.color = hex;  
+        }
+    }  
+}
